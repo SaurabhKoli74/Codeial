a1de6157c55e5b1c5af4d4594600521804ade390
Saurabh <saurabh.koli7420@gmail.com>
2023-01-27 16:31:46 +0530

    added user_controller and a router for users

---
+++ routes/users.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+const usersController = require('../controllers/users_Controller');
+
+router.get('/profile',usersController.profile);
+module.exports = router;
