b1f5e62efea696224ecac7467a44fa299bf843b8
Saurabh <saurabh.koli7420@gmail.com>
2023-02-03 20:14:46 +0530

    Implemented Partials and Layouts. Installed express-ejs-layouts library for implementing Layouts

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const expressEjsLayouts = require('express-ejs-layouts');
 const app = express();
 const port = 8000;
+
+app.use(expressEjsLayouts);
+
+
 
 //use express router
 //by default it takes './routes/index' so it is necessary to crete index.js in routes otherwise explicitally we have to mention it like ./routes/about so control will start from about 
@@ -15,6 +20,7 @@ app.listen(port, function (err) {
         // console.log("Error: ",err);
         console.log(`Error in running the server: ${err}`);
         //include var inside string is called interpolation
+        return ;
     }
 
     console.log(`Server is running on port: ${port}`);
