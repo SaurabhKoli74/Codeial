793f8ab3d85044f4642938b701aab84fc231f20e
Saurabh <saurabh.koli7420@gmail.com>
2023-02-10 11:10:18 +0530

    Rendering pages for sign-in & sign-up

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,9 @@ const router = express.Router();
 
 const usersController = require('../controllers/users_Controller');
 
-router.get('/',usersController.profile)
+
 router.get('/profile',usersController.profile);
+router.get('/sign-up',usersController.signUp);
+router.get('/sign-in',usersController.signIn);
+
 module.exports = router;
