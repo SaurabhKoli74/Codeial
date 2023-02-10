290cefafe8d69625cecba8beba5a2fb12ae06ea6
Saurabh <saurabh.koli7420@gmail.com>
2023-02-10 11:22:05 +0530

    Creating forms for Sign-In and Sign-Up

--- controllers/users_Controller.js
+++ controllers/users_Controller.js
@@ -16,3 +16,15 @@ module.exports.signIn = function(req,res){
         title:"Codeial | Sign In"
     });
 }
+
+
+//get the sign up data
+module.exports.create = function(req,res){
+    //TODO letter
+}
+
+
+//sign in and create a session for the user
+module.exports.createSession = function(req,res){
+    //TODO later
+}
