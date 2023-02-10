e8242b02c7a19f330c8c1990247a8f6e14b9dcca
Saurabh <saurabh.koli7420@gmail.com>
2023-02-10 18:10:10 +0530

    Install Mongo-store and configure it for storing session permentally into DB. It resolves the drawback as previously when server restarts it sign out automatically basically it was deleting the cookie when server restarts

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal =require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo')(session); //Downgraded connect-mongo version to downgrade it to 3
+//command - npm uninstall connect-mongo
+// npm i connect-mongo@3
+
+
 
 app.use(express.urlencoded());
 
@@ -35,6 +40,8 @@ app.set('layout extractScripts',true);
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+
+//mongo store is used to store the session cookie in the db
 app.use(session({
     name:'codeial',
     //TODO change the secret before deployment in production mode
@@ -43,7 +50,15 @@ app.use(session({
     resave:false,
     cookie:{
         maxAge:(1000*60*100)
+    },
+    store:new MongoStore({
+        mongooseConnection:db,
+        autoRemove:'disabled'
+    },
+    function(error){
+        console.log(err || 'connect-mongodb setup ok');
     }
+    )
 }));
 
 
