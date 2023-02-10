c0577b203f2c80425ddb2a6f14c0f5c2a42043a0
Saurabh <saurabh.koli7420@gmail.com>
2023-02-10 13:47:59 +0530

    1st Step - Create User (Sign-Up) Done

--- routes/users.js
+++ routes/users.js
@@ -9,5 +9,5 @@ const usersController = require('../controllers/users_Controller');
 router.get('/profile',usersController.profile);
 router.get('/sign-up',usersController.signUp);
 router.get('/sign-in',usersController.signIn);
-
+router.post('/create',usersController.create);
 module.exports = router;
